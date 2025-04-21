5418cd138635662bfb445c1ab9419699d3c01972
Callum Keogan <48253321+calkeo@users.noreply.github.com>
2025-04-21 14:59:22 +0100

    Restore changelog and rename to stop it being deleted

--- app/Commands/ChangelogCommand.php
+++ app/Commands/ChangelogCommand.php
@@ -24,7 +24,7 @@ class ChangelogCommand extends AbstractCommand
         $output = [];
 
         // Path to the changelog file
-        $changelogPath = base_path('CHANGELOG.md');
+        $changelogPath = base_path('APP_CHANGELOG.md');
 
         // Check if the file exists
         if (!file_exists($changelogPath)) {
